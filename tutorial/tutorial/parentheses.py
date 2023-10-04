6b0aa6a31e1bb0c605c14f0a50b5a37f0fc71fc2
Petrosmts <petrosmatsoukes@gmail.com>
2023-10-04 13:33:13 +0300

    changes

--- tutorial/tutorial/parentheses.py
+++ tutorial/tutorial/parentheses.py
@@ -17,18 +17,20 @@ class Stack:
     def pop(self):  #removes the top element
         self.dlist.pop(len(self.dlist) - 1)
     
-    
-stack1 = Stack()  #we create a stack named stack1
-a = input('Give a string:')
-for i in range(len(a)):  #access to all elements of the string one by one
-    if a[i] == '(' or a[i] == '[' or a[i] == '{':  #if element is '(', '[' or '{' then we push it in the stack
-        stack1.push(a[i])
-    elif (stack1.top() == '(' and a[i] == ')') or (stack1.top() == '[' and a[i] == ']') or ( stack1.top() == '{' and a[i] == '}'):
-        #if we find the type that matches the type of the top stack1 element, then pop the top element out of stack1
-        stack1.pop()
-#if stack1 is empty then all characters found their matching type character with the right order, so they are well balanced
-if stack1.empty():
-    print("Characters are well balanced")
-else:
-    print("Characters are not well balanced")
+def main():
+    stack1 = Stack()  #we create a stack named stack1
+    a = input('Give a string:')
+    for i in range(len(a)):  #access to all elements of the string one by one
+        if a[i] == '(' or a[i] == '[' or a[i] == '{':  #if element is '(', '[' or '{' then we push it in the stack
+            stack1.push(a[i])
+        elif (stack1.top() == '(' and a[i] == ')') or (stack1.top() == '[' and a[i] == ']') or ( stack1.top() == '{' and a[i] == '}'):
+            #if we find the type that matches the type of the top stack1 element, then pop the top element out of stack1
+            stack1.pop()
+    #if stack1 is empty then all characters found their matching type character with the right order, so they are well balanced
+    if stack1.empty():
+        print("Characters are well balanced")
+    else:
+        print("Characters are not well balanced")
 
+if __name__ == '__main__':
+    main()
